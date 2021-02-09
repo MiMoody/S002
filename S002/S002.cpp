cb831944f10020da9069e0e4ac2d22fb6b877a18
Тема Шевченко <svev2369@gmail.com>
2021-02-09 12:48:15 +0700

    Добавьте файлы проекта.

---
+++ S002/S002.cpp
@@ -0,0 +1,83 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    char str1[] = "аааа";
+    char str2[] = "аааб";
+    char Zero[] = "0";
+    char One[] = "1";
+    char MinusOne[] = "-1";
+    char fd[] = "%s";
+    __asm
+    {
+        lea ebx, str1
+
+        lea ecx, str2
+
+        dec ecx
+
+        dec ebx
+
+        FirstCompare:
+                inc ebx
+                inc ecx
+                mov al,[ebx]
+                mov dl,[ecx]
+                cmp al,0
+                je SecondCompare
+                cmp dl,0
+                je OutputMinusOne
+                cmp al,dl
+                je FirstCompare
+                jb OutputOne
+                jmp OutputMinusOne
+        SecondCompare:
+            cmp dl,0
+            je OutputZero
+            jmp OutputOne
+
+        OutputZero:
+            lea ebx,Zero
+            push ebx
+            lea ecx, fd
+            push ecx
+            call printf
+            add esp,8
+            jmp End
+
+        OutputOne:
+            lea ebx, One
+            push ebx
+            lea ecx, fd
+            push ecx
+            call printf
+            add esp, 8
+            jmp End
+
+        OutputMinusOne:
+            lea ebx, MinusOne
+            push ebx
+            lea ecx, fd
+            push ecx
+            call printf
+            add esp, 8
+            jmp End
+
+        End:
+
+
+            
+
+
+
+        
+
+
+    }
+   
+
+   
+}
+
